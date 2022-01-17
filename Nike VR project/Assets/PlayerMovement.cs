037c6b51b6bc04a9b211061807fd0ee0523dabec
Amalhotra2602 <97895985+Amalhotra2602@users.noreply.github.com>
2022-01-17 14:29:07 +0000

    new project

---
+++ Nike VR project/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    //[Serializefield] float rotatespeed;
+
+    //Rigidbody rigidbody;
+
+    //Void Start()
+    //{
+    //    myrigidbody = GetComponent<rigidbody>();
+
+    //}
+
+    //void update()
+    //{
+    //    rotate();
+    
+
+    //public void rotate()
+    //    {
+    //        float inputAxis = Input.GetAxis("Vertical");
+    //        transform.Rotate(Vector3.back * inputAxis * rotatespeed); 
+
+        //}
+}
